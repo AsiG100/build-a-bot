91828d8e4b9bdd594a9f07aa7681ab841612d226
asafg <asafg@aspireglobal.com>
2019-06-20 17:20:35 +0300

    First comit

---
+++ src/router/index.js
@@ -0,0 +1,53 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+
+import HomePage from '../home/HomePage.vue';
+import RobotBuilder from '../build/RobotBuilder.vue';
+import BrowseParts from '../parts/BrowseParts.vue';
+import PartInfo from '../parts/PartInfo.vue';
+
+Vue.use(Router);
+
+export default new Router({
+  routes: [{
+    path: '/',
+    name: 'Home',
+    component: HomePage,
+  }, {
+    path: '/build',
+    name: 'Build',
+    component: RobotBuilder,
+  }, {
+    path: '/parts/browse',
+    name: 'BrowseParts',
+    component: BrowseParts,
+    props: true,
+    children: [
+        {
+          name: 'BrowseHeads',
+          path: 'heads',
+          component: RobotHeads,
+        },
+        {
+          name: 'BrowseArms',
+          path: 'arms',
+          component: RobotArms,
+        },
+        {
+          name: 'BrowseTorsos',
+          path: 'torsos',
+          component: RobotTorsos,
+        },
+        {
+          name: 'BrowseBass',
+          path: 'bases',
+          component: RobotBases,
+        },
+    ],
+  }, {
+    path: '/parts/:partType/:id',
+    name: 'Parts',
+    component: PartInfo,
+    props: true,
+  }],
+});
